8a8ef222d330522859c9edca09037d0e56388096
dimowsl <127833200+dimowsl@users.noreply.github.com>
2024-03-27 19:29:59 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace DimPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuRedo = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFormat = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuView = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuWordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuPageSetup = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPrint = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuEdit,
+            this.menuView});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // menuEdit
+            // 
+            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuUndo,
+            this.menuRedo,
+            this.toolStripSeparator4,
+            this.menuCut,
+            this.menuCopy,
+            this.menuPaste,
+            this.menuSelectAll,
+            this.menuFormat,
+            this.fontToolStripMenuItem});
+            this.menuEdit.Image = global::TextPadSotirios.Properties.Resources._686112;
+            this.menuEdit.Name = "menuEdit";
+            this.menuEdit.Size = new System.Drawing.Size(55, 20);
+            this.menuEdit.Text = "&Edit";
+            // 
+            // menuUndo
+            // 
+            this.menuUndo.Name = "menuUndo";
+            this.menuUndo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.menuUndo.Size = new System.Drawing.Size(180, 22);
+            this.menuUndo.Text = "Undo";
+            this.menuUndo.Click += new System.EventHandler(this.menuUndo_Click);
+            // 
+            // menuRedo
+            // 
+            this.menuRedo.Name = "menuRedo";
+            this.menuRedo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
+            this.menuRedo.Size = new System.Drawing.Size(180, 22);
+            this.menuRedo.Text = "Redo";
+            this.menuRedo.Click += new System.EventHandler(this.menuRedo_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menuCut
+            // 
+            this.menuCut.Name = "menuCut";
+            this.menuCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.menuCut.Size = new System.Drawing.Size(180, 22);
+            this.menuCut.Text = "Cut";
+            this.menuCut.Click += new System.EventHandler(this.menuCut_Click);
+            // 
+            // menuCopy
+            // 
+            this.menuCopy.Name = "menuCopy";
+            this.menuCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.menuCopy.Size = new System.Drawing.Size(180, 22);
+            this.menuCopy.Text = "Copy";
+            this.menuCopy.Click += new System.EventHandler(this.menuCopy_Click);
+            // 
+            // menuPaste
+            // 
+            this.menuPaste.Name = "menuPaste";
+            this.menuPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.menuPaste.Size = new System.Drawing.Size(180, 22);
+            this.menuPaste.Text = "Paste";
+            this.menuPaste.Click += new System.EventHandler(this.menuPaste_Click);
+            // 
+            // menuSelectAll
+            // 
+            this.menuSelectAll.Name = "menuSelectAll";
+            this.menuSelectAll.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.menuSelectAll.Size = new System.Drawing.Size(180, 22);
+            this.menuSelectAll.Text = "Select All";
+            this.menuSelectAll.Click += new System.EventHandler(this.menuSelectAll_Click);
+            // 
+            // menuFormat
+            // 
+            this.menuFormat.Name = "menuFormat";
+            this.menuFormat.Size = new System.Drawing.Size(180, 22);
+            this.menuFormat.Text = "Format";
+            this.menuFormat.Click += new System.EventHandler(this.menuFormat_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // menuView
+            // 
+            this.menuView.Checked = true;
+            this.menuView.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.menuView.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuWordWrap});
+            this.menuView.Image = global::TextPadSotirios.Properties.Resources._2389603;
+            this.menuView.Name = "menuView";
+            this.menuView.Size = new System.Drawing.Size(60, 20);
+            this.menuView.Text = "&View";
+            // 
+            // menuWordWrap
+            // 
+            this.menuWordWrap.Checked = true;
+            this.menuWordWrap.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.menuWordWrap.Name = "menuWordWrap";
+            this.menuWordWrap.Size = new System.Drawing.Size(180, 22);
+            this.menuWordWrap.Text = "Word Wrap";
+            this.menuWordWrap.Click += new System.EventHandler(this.wordWrapToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 426);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuNew,
+            this.menuOpen,
+            this.toolStripSeparator1,
+            this.menuSave,
+            this.menuSaveAs,
+            this.toolStripSeparator3,
+            this.menuPageSetup,
+            this.menuPrint,
+            this.toolStripSeparator2,
+            this.menuExit});
+            this.menuFile.Image = global::TextPadSotirios.Properties.Resources.document_icon_191;
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(53, 20);
+            this.menuFile.Text = "&File";
+            this.menuFile.Click += new System.EventHandler(this.menuFile_Click);
+            // 
+            // menuNew
+            // 
+            this.menuNew.Name = "menuNew";
+            this.menuNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuNew.Size = new System.Drawing.Size(180, 22);
+            this.menuNew.Text = "New";
+            this.menuNew.Click += new System.EventHandler(this.menuNew_Click);
+            // 
+            // menuOpen
+            // 
+            this.menuOpen.Name = "menuOpen";
+            this.menuOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.menuOpen.Size = new System.Drawing.Size(180, 22);
+            this.menuOpen.Text = "Open";
+            this.menuOpen.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.menuOpen.Click += new System.EventHandler(this.menuOpen_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menuSave
+            // 
+            this.menuSave.Name = "menuSave";
+            this.menuSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.menuSave.Size = new System.Drawing.Size(180, 22);
+            this.menuSave.Text = "Save";
+            this.menuSave.Click += new System.EventHandler(this.menuSave_Click);
+            // 
+            // menuSaveAs
+            // 
+            this.menuSaveAs.Name = "menuSaveAs";
+            this.menuSaveAs.Size = new System.Drawing.Size(180, 22);
+            this.menuSaveAs.Text = "Save as";
+            this.menuSaveAs.Click += new System.EventHandler(this.menuSaveAs_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menuPageSetup
+            // 
+            this.menuPageSetup.Name = "menuPageSetup";
+            this.menuPageSetup.Size = new System.Drawing.Size(180, 22);
+            this.menuPageSetup.Text = "Page setup";
+            this.menuPageSetup.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // menuPrint
+            // 
+            this.menuPrint.Name = "menuPrint";
+            this.menuPrint.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.menuPrint.Size = new System.Drawing.Size(180, 22);
+            this.menuPrint.Text = "Print";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menuExit
+            // 
+            this.menuExit.Name = "menuExit";
+            this.menuExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.menuExit.Size = new System.Drawing.Size(180, 22);
+            this.menuExit.Text = "Exit";
+            this.menuExit.Click += new System.EventHandler(this.menuExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MyTextPad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem menuNew;
+        private System.Windows.Forms.ToolStripMenuItem menuOpen;
+        private System.Windows.Forms.ToolStripMenuItem menuSave;
+        private System.Windows.Forms.ToolStripMenuItem menuSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem menuPrint;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit;
+        private System.Windows.Forms.ToolStripMenuItem menuView;
+        private System.Windows.Forms.ToolStripMenuItem menuExit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem menuPageSetup;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem menuUndo;
+        private System.Windows.Forms.ToolStripMenuItem menuRedo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem menuCut;
+        private System.Windows.Forms.ToolStripMenuItem menuCopy;
+        private System.Windows.Forms.ToolStripMenuItem menuPaste;
+        private System.Windows.Forms.ToolStripMenuItem menuSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem menuFormat;
+        private System.Windows.Forms.ToolStripMenuItem menuWordWrap;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DimPad
+{
+    public partial class Form1 : Form
+    {
+        string lastEditedFile = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wordWrapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuWordWrap.Checked = !menuWordWrap.Checked;
+            if (menuWordWrap.Checked)
+            {
+                richTextBox1.WordWrap = true;
+            }
+            else
+            {
+                richTextBox1.WordWrap = false;
+            }
+        }
+
+        private void menuNew_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult answer = MessageBox.Show("Do you want to save the changes",
+                "Confirmation", MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                if (answer == DialogResult.Yes)
+                {
+                    menuSave_Click(sender, e);
+                }
+                if (answer == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            richTextBox1.Clear();
+            lastEditedFile = "";
+            this.Text = "DimPad - New File";
+            richTextBox1.Modified = false;
+        }
+
+        private void menuOpen_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult answer = MessageBox.Show("Do you want to save the changes",
+                "Confirmation", MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                if (answer == DialogResult.Yes)
+                {
+                    menuSave_Click(sender, e);
+                }
+                if (answer == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            //1
+            openFileDialog1.Filter = "Text files|*.txt|Rich Text Files|*.rtf|Word files|*.doc;*.docx|All files|*.*";
+            openFileDialog1.FileName = "";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.Title = "Open in Text Pad";
+            //2.
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            //3.
+            lastEditedFile = openFileDialog1.FileName;
+            this.Text = "DimPad - " + openFileDialog1.SafeFileName;
+            if (openFileDialog1.FilterIndex == 1)
+            {
+               richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+            else if (openFileDialog1.FilterIndex == 2)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+            }
+            else
+            {
+                MessageBox.Show("raboti samo s TXT ili RTF");
+                return;
+            }
+            richTextBox1.Modified = false;
+
+
+        }
+
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuPaste_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void menuUndo_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void menuRedo_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void menuCut_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void menuCopy_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void menuSelectAll_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wordWrapToolStripMenuItem_Click(sender, e);
+        }
+
+        private void menuSaveAs_Click(object sender, EventArgs e)
+        {
+            //1
+            saveFileDialog1.Filter = "Text files|*.txt|Rich Text Files|*.rtf|Word files|*.docx";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.Title = "Open in Text Pad";
+            //2.
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            //3.
+            lastEditedFile = saveFileDialog1.FileName;
+            this.Text = "Text Pad -" + saveFileDialog1.FileName;
+            if (saveFileDialog1.FilterIndex == 2)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+            }
+            else if (saveFileDialog1.FilterIndex == 1)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+            else
+            {
+                MessageBox.Show("Ne raboti s tozi format");
+                return;
+            }
+            richTextBox1.Modified = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //1
+            fontDialog1.ShowColor = true;
+            //2
+            fontDialog1.ShowDialog();
+            //3
+            richTextBox1.SelectionFont = fontDialog1.Font;
+            richTextBox1.SelectionColor = fontDialog1.Color;
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult answer =
+                MessageBox.Show("Do you want to  save the changes ?",
+                "Confirmation",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1);
+                if (answer == DialogResult.Yes)
+                {
+                    menuSave_Click(sender, e);
+                }
+                if (answer == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void menuSave_Click(object sender, EventArgs e)
+        {
+            if (lastEditedFile == "")
+            {
+                menuSaveAs_Click(sender, e);
+            }
+            else
+            {
+                
+                richTextBox1.SaveFile(lastEditedFile, RichTextBoxStreamType.RichText);
+               
+
+            }
+            richTextBox1.Modified = false;
+        }
+
+        private void menuFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuFormat_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
